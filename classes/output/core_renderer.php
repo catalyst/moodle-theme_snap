066eda599a528dc930fe788b139b7385e9532778
Alex Damsted <alexdamsted@catalyst-au.net>
2025-08-18 16:51:10 +0930

    Syntax fixing

--- classes/output/core_renderer.php
+++ classes/output/core_renderer.php
@@ -163,8 +163,8 @@ class core_renderer extends \theme_boost\output\core_renderer {
         $snapfeedsurlparam = isset($CFG->theme_snap_feeds_url_parameter) ? $CFG->theme_snap_feeds_url_parameter : true;
         if ($location == 'snapfeedsmenu') {
             if ($snapfeedsurlparam) {
-		if (strpos($langstring, 'viewmessaging') !== false) ||
-    (strpos($langstring, 'viewmyfeedback') !== false) {
+		if ((strpos($langstring, 'viewmessaging') !== false) ||
+    (strpos($langstring, 'viewmyfeedback') !== false)) {
 		    $link = '<a class="snap-feeds-menu-more" href="' .$url. '?snapfeedsclicked=on" title="'.$text.'"><small>' .$text. '</small>' .$icon. '</a>'; 
                 } else {
                     $link = '<a class="snap-feeds-menu-more" href="' .$url. '&snapfeedsclicked=on" title="'.$text.'"><small>' .$text. '</small>' .$icon. '</a>';
